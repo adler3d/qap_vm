34c74851591cfd0b3274e2cd2f43d36962cc88c8
adler3d <adler3d@gmail.com>
2025-05-25 10:04:17 +0300

    Update main.cpp

--- trash/test2025/wasm_test/main.cpp
+++ trash/test2025/wasm_test/main.cpp
@@ -4,6 +4,48 @@ using namespace std;
 typedef double real;
 template<typename TYPE>inline TYPE Lerp(const TYPE&A,const TYPE&B,const real&v){return A+(B-A)*v;}
 template<class TYPE>inline TYPE Clamp(const TYPE&v,const TYPE&a,const TYPE&b){return max(a,min(v, b));}
+struct vec2d
+{
+public:
+  real x,y;
+  vec2d():x(0),y(0){}
+  vec2d(const real&x,const real&y):x(x),y(y){}
+  vec2d(const vec2d&v):x(v.x),y(v.y){}
+  vec2d&operator=(const vec2d&v){x=v.x;y=v.y;return *this;}
+  vec2d operator+()const{return *this;}
+  vec2d operator-()const{return vec2d(-x,-y);}
+  vec2d&operator+=(const vec2d&v){x+=v.x;y +=v.y;return *this;}
+  vec2d&operator-=(const vec2d&v){x-=v.x; y-=v.y;return *this;}
+  vec2d&operator*=(const real&f){x*=f;y*=f;return *this;}
+  vec2d&operator/=(const real&f){x/=f;y/=f;return *this;}
+public:
+  vec2d Rot(const vec2d&OX)const{real M=OX.Mag();return vec2d(((x*+OX.x)+(y*OX.y))/M,((x*-OX.y)+(y*OX.x))/M);}
+  vec2d UnRot(const vec2d&OX)const{real M=OX.Mag();if(M==0.0f){return vec2d(0,0);};return vec2d(((x*OX.x)+(y*-OX.y))/M,((x*OX.y)+(y*+OX.x))/M);}
+  vec2d Ort()const{return vec2d(-y,x);}
+  vec2d Norm()const{if((x==0)&&(y==0)){return vec2d(0,0);}return vec2d(x/this->Mag(),y/this->Mag());}
+  vec2d SetMag(const real&val)const{return this->Norm()*val;}
+  vec2d Mul(const vec2d&v)const{return vec2d(x*v.x,y*v.y);}
+  vec2d Div(const vec2d&v)const{return vec2d(v.x!=0?x/v.x:x,v.y!=0?y/v.y:y);}
+  real GetAng()const{return atan2(y,x);}
+  real Mag()const{return sqrt(x*x+y*y);}
+public:
+  friend vec2d operator+(const vec2d&u,const vec2d&v){return vec2d(u.x+v.x,u.y+v.y);}
+  friend vec2d operator-(const vec2d&u,const vec2d&v){return vec2d(u.x-v.x,u.y-v.y);}
+  friend vec2d operator*(const vec2d&u,const real&v){return vec2d(u.x*v,u.y*v);}
+  friend vec2d operator*(const real&u,const vec2d&v){return vec2d(u*v.x,u*v.y);}
+  friend bool operator==(const vec2d&u,const vec2d&v){return (u.x==v.x)||(u.y==v.y);}
+  friend bool operator!=(const vec2d&u,const vec2d&v){return (u.x!=v.x)||(u.y!=v.y);}
+public:
+  //friend inline static vec2d Vec2dEx(const real&ang,const real&mag){return vec2d(cos(ang)*mag,sin(ang)*mag);}
+public:
+  friend inline static real dot(const vec2d&a,const vec2d&b){return a.x*b.x+a.y*b.y;}
+  friend inline static real cross(const vec2d&a,const vec2d&b){return a.x*b.y-a.y*b.x;}
+public:
+  #ifdef BOX2D_H
+    operator b2Vec2()const{return b2Vec2(x,y);}
+    vec2d(const b2Vec2& v):x(v.x),y(v.y){}
+  #endif
+};
 struct QapColor{
 public:
   typedef unsigned char byte;
