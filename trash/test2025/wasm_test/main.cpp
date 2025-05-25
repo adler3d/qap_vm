f36dcbf597bea255a8c5124f1ed8d8c374b49d2b
adler3d <adler3d@gmail.com>
2025-05-25 10:01:02 +0300

    Update main.cpp

--- trash/test2025/wasm_test/main.cpp
+++ trash/test2025/wasm_test/main.cpp
@@ -1,8 +1,8 @@
 #include <emscripten.h>
 #include <vector>
 using namespace std;
-typdef double real;
-typdef unsigned char byte;
+typedef double real;
+typedef unsigned char byte;
 template<typename TYPE>inline TYPE Lerp(const TYPE&A,const TYPE&B,const real&v){return A+(B-A)*v;}
 template<class TYPE>inline TYPE Clamp(const TYPE&v,const TYPE&a,const TYPE&b){return max(a,min(v, b));}
 struct QapColor
