dfc0732ba09cafe3d1b384101c100a51e47348ad
adler3d <adler3d@gmail.com>
2025-05-25 08:19:27 +0300

    Update main.cpp

--- trash/test2025/wasm_test/main.cpp
+++ trash/test2025/wasm_test/main.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 int main() {
-  vector<int> V={1,2,3};
+  vector<int> V={10,20,30};
   EM_ASM(document.body.innerHTML='<canvas id="glcanvas" width="1920" height="1024"></canvas><hr>';);
   EM_ASM({
     console.log('I received: ' + $0);
@@ -12,6 +12,10 @@ int main() {
       let b=HEAP32[(g_data>>2)+1];
       let c=HEAP32[(g_data>>2)+2];
       console.log({a,b,c});
+      let x=HEAP8[g_data+0];
+      let y=HEAP8[g_data+1];
+      let z=HEAP8[g_data+2];
+      console.log({x,y,z});
     }
   }, int(V.data()));
   EM_ASM(main(););
