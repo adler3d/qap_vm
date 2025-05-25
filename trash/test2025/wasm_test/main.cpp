35fffa102427b6a9c3b8f7cad90208b3708fdb66
adler3d <adler3d@gmail.com>
2025-05-25 13:25:59 +0300

    Update main.cpp

--- trash/test2025/wasm_test/main.cpp
+++ trash/test2025/wasm_test/main.cpp
@@ -909,15 +909,16 @@ int main() {
       qDev.parr.length=g_VBN*2;
       qDev.carr.length=g_VBN*4;
       qDev.tarr.length=g_VBN*2;
+      let inv_255=1.0/255;
       for(let i=0;i<g_VBN;i++){
         // vec3f,uint,vec2f -> 32*3,32,32*2 -> 32*6
         qDev.parr[i*2+0]=HEAPF32[(g_VB>>2)+i*6+0];
         qDev.parr[i*2+1]=HEAPF32[(g_VB>>2)+i*6+1];
         //qDev.parr[i*3+2]=HEAPF32[(g_VB>>2)+i*6+2];
-        qDev.carr[i*4+0]=HEAPU8[g_VB+i*6*4+3*4+0];
-        qDev.carr[i*4+1]=HEAPU8[g_VB+i*6*4+3*4+1];
-        qDev.carr[i*4+2]=HEAPU8[g_VB+i*6*4+3*4+2];
-        qDev.carr[i*4+3]=HEAPU8[g_VB+i*6*4+3*4+3];
+        qDev.carr[i*4+0]=HEAPU8[g_VB+i*6*4+3*4+0]*inv_255;
+        qDev.carr[i*4+1]=HEAPU8[g_VB+i*6*4+3*4+1]*inv_255;
+        qDev.carr[i*4+2]=HEAPU8[g_VB+i*6*4+3*4+2]*inv_255;
+        qDev.carr[i*4+3]=HEAPU8[g_VB+i*6*4+3*4+3]*inv_255;
         qDev.tarr[i*2+0]=HEAPF32[(g_VB>>2)+i*6+3+1+0];
         qDev.tarr[i*2+1]=HEAPF32[(g_VB>>2)+i*6+3+1+1];
       }
