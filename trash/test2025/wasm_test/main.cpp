dc75a0499f3bf62a338ae777ef77e831c64367c8
adler3d <adler3d@gmail.com>
2025-05-26 22:04:38 +0300

    Update main.cpp

--- trash/test2025/wasm_test/main.cpp
+++ trash/test2025/wasm_test/main.cpp
@@ -3548,7 +3548,7 @@ extern "C" {
     qDev.color=0xFFffFFff;
     */
     qDev.color=0xFF000000;
-    qDev.BindTex(0,BlurFont.Tex);
+    qDev.BindTex(0,0);//BlurFont.Tex);
     qDev.DrawQuad(-500+1.5,-1.5,-512,512,Pi);
     qDev.BindTex(0,NormFont.Tex);
     qDev.color=0xFFffFFff;
