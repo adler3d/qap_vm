58f5cf85dddc258946e6e733806ee3efff64234e
adler3d <adler3d@gmail.com>
2025-05-27 19:56:29 +0300

    Update webgl-demo.js

--- trash/test2025/wasm_test/webgl-demo.js
+++ trash/test2025/wasm_test/webgl-demo.js
@@ -186,6 +186,7 @@ function initFont(fontSize,fontFamily,texSize,bold){
   }
   out.data=ctx.getImageData(0,0,texSize,texSize).data;
   out.ctx=ctx;
+  canvas.remove();
   return out;
 }
 function initFont_v2(font,dest,pW,pH){
@@ -266,6 +267,7 @@ function drawScene(gl,prog,buffers,squareRotation,texture) {
     false,
     modelViewMatrix
   );
+  gl.uniform1f(prog.uniformLocations.use_tex,1.0);
   gl.activeTexture(gl.TEXTURE0);
   gl.bindTexture(gl.TEXTURE_2D,texture);
   gl.uniform1i(prog.uniformLocations.uSampler,0);
@@ -318,9 +320,10 @@ function main() {
     varying highp vec4 vColor;
     varying highp vec2 vTextureCoord;
     uniform sampler2D uSampler;
+    uniform float use_tex;
 
     void main(void) {
-      vec4 t=texture2D(uSampler,vTextureCoord);
+      vec4 t=use_tex==1.0?texture2D(uSampler,vTextureCoord):vec4(1,1,1,1);
       gl_FragColor = t*vColor;
       gl_FragColor.rgb *= gl_FragColor.a;
     }
@@ -339,13 +342,14 @@ function main() {
       projectionMatrix:gl.getUniformLocation(shaderProgram,"uProjectionMatrix"),
       modelViewMatrix: gl.getUniformLocation(shaderProgram,"uModelViewMatrix"),
       uSampler:        gl.getUniformLocation(shaderProgram,"uSampler"),
+      use_tex:         gl.getUniformLocation(shaderProgram,"use_tex"),
     },
   };
 
   qDev.gl=gl;
 
-  const texture = loadTexture(gl, "cubetexture.png");
-  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+  const texture=loadTexture(gl,"cubetexture.png");
+  //gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL,true);
 
   let then = 0;
 
@@ -353,14 +357,207 @@ function main() {
     now *= 0.001;
     deltaTime = now - then;
     then = now;
-    drawScene(gl, prog, null, squareRotation,texture);
+    drawScene(gl,prog,null,squareRotation,texture);
     //squareRotation += deltaTime;
     requestAnimationFrame(render);
   }
   requestAnimationFrame(render);
 }
-
+function update_kb(pD,pC){
+  let MAX_KEY=263;
+  for(let i=0;i<=MAX_KEY;i++){
+    HEAPU8[pD+i]=g_kb_down[i];
+    HEAPU8[pC+i]=g_kb_changed[i];
+    g_kb_changed[i]=false;
+  }
+}
+//https://gist.github.com/adler3d/7f18282ea9ef553d5fa03a101bf4c3d0
+var g_keys={"0":{"v":["VK_NUMPAD0"],"k":[96]},
+"1":{"v":["VK_NUMPAD1"],"k":[97]},
+"2":{"v":["VK_NUMPAD2"],"k":[98]},
+"3":{"v":["VK_NUMPAD3"],"k":[99]},
+"4":{"v":["VK_NUMPAD4"],"k":[100]},
+"5":{"v":["VK_NUMPAD5"],"k":[101]},
+"6":{"v":["VK_NUMPAD6"],"k":[102]},
+"7":{"v":["VK_NUMPAD7"],"k":[103]},
+"8":{"v":["VK_NUMPAD8"],"k":[104]},
+"9":{"v":["VK_NUMPAD9"],"k":[105]},
+"Alt":{"v":["VK_MENU","VK_LMENU","VK_RMENU"],"k":[18,164,165]},
+"CapsLock":{"v":["VK_CAPITAL"],"k":[20]},
+"Control":{"v":["VK_CONTROL","VK_LCONTROL","VK_RCONTROL"],"k":[17,162,163]},
+"Meta":{"v":["VK_LWIN","VK_RWIN"],"k":[91,92]},
+"NumLock":{"v":["VK_NUMLOCK"],"k":[144]},
+"ScrollLock":{"v":["VK_SCROLL"],"k":[145]},
+"Shift":{"v":["VK_SHIFT","VK_LSHIFT","VK_RSHIFT"],"k":[16,160,161]},
+"Enter":{"v":["VK_RETURN"],"k":[13]},
+"Tab":{"v":["VK_TAB"],"k":[9]},
+" ":{"v":["VK_SPACE"],"k":[32]},
+"ArrowDown":{"v":["VK_DOWN"],"k":[40]},
+"ArrowLeft":{"v":["VK_LEFT"],"k":[37]},
+"ArrowRight":{"v":["VK_RIGHT"],"k":[39]},
+"ArrowUp":{"v":["VK_UP"],"k":[38]},
+"End":{"v":["VK_END"],"k":[35]},
+"Home":{"v":["VK_HOME"],"k":[36]},
+"PageDown":{"v":["VK_NEXT"],"k":[34]},
+"PageUp":{"v":["VK_PRIOR"],"k":[33]},
+"Backspace":{"v":["VK_BACK"],"k":[8]},
+"Clear":{"v":["VK_CLEAR","VK_OEM_CLEAR"],"k":[12,254]},
+"Copy":{"v":["APPCOMMAND_COPY"],"k":[36]},
+"CrSel":{"v":["VK_CRSEL"],"k":[247]},
+"Cut":{"v":["APPCOMMAND_CUT"],"k":[37]},
+"Delete":{"v":["VK_DELETE"],"k":[46]},
+"EraseEof":{"v":["VK_EREOF"],"k":[249]},
+"ExSel":{"v":["VK_EXSEL"],"k":[248]},
+"Insert":{"v":["VK_INSERT"],"k":[45]},
+"Paste":{"v":["APPCOMMAND_PASTE"],"k":[38]},
+"Redo":{"v":["APPCOMMAND_REDO"],"k":[35]},
+"Undo":{"v":["APPCOMMAND_UNDO"],"k":[34]},
+"Accept":{"v":["VK_ACCEPT"],"k":[30]},
+"Attn":{"v":["VK_OEM_ATTN"],"k":[240]},
+"ContextMenu":{"v":["VK_APPS"],"k":[93]},
+"Escape":{"v":["VK_ESCAPE"],"k":[27]},
+"Execute":{"v":["VK_EXECUTE"],"k":[43]},
+"Find":{"v":["APPCOMMAND_FIND"],"k":[28]},
+"Finish":{"v":["VK_OEM_FINISH"],"k":[241]},
+"Help":{"v":["VK_HELP","APPCOMMAND_HELP"],"k":[47,27]},
+"Pause":{"v":["VK_PAUSE"],"k":[19]},
+"Play":{"v":["VK_PLAY"],"k":[250]},
+"Select":{"v":["VK_SELECT"],"k":[41]},
+"PrintScreen":{"v":["VK_SNAPSHOT"],"k":[44]},
+"Standby":{"v":["VK_SLEEP"],"k":[95]},
+"Alphanumeric":{"v":["VK_OEM_ATTN"],"k":[240]},
+"Convert":{"v":["VK_CONVERT"],"k":[28]},
+"FinalMode":{"v":["VK_FINAL"],"k":[24]},
+"ModeChange":{"v":["VK_MODECHANGE"],"k":[31]},
+"NonConvert":{"v":["VK_NONCONVERT"],"k":[29]},
+"Process":{"v":["VK_PROCESSKEY"],"k":[229]},
+"HangulMode":{"v":["VK_HANGUL"],"k":[21]},
+"HanjaMode":{"v":["VK_HANJA"],"k":[25]},
+"JunjaMode":{"v":["VK_JUNJA"],"k":[23]},
+"Hankaku":{"v":["VK_OEM_AUTO"],"k":[243]},
+"Hiragana":{"v":["VK_OEM_COPY"],"k":[242]},
+"KanaMode":{"v":["VK_KANA","VK_ATTN"],"k":[21,246]},
+"KanjiMode":{"v":["VK_KANJI"],"k":[25]},
+"Katakana":{"v":["VK_OEM_FINISH"],"k":[241]},
+"Romaji":{"v":["VK_OEM_BACKTAB"],"k":[245]},
+"Zenkaku":{"v":["VK_OEM_ENLW"],"k":[244]},
+"F1":{"v":["VK_F1"],"k":[112]},
+"F2":{"v":["VK_F2"],"k":[113]},
+"F3":{"v":["VK_F3"],"k":[114]},
+"F4":{"v":["VK_F4"],"k":[115]},
+"F5":{"v":["VK_F5"],"k":[116]},
+"F6":{"v":["VK_F6"],"k":[117]},
+"F7":{"v":["VK_F7"],"k":[118]},
+"F8":{"v":["VK_F8"],"k":[119]},
+"F9":{"v":["VK_F9"],"k":[120]},
+"F10":{"v":["VK_F10"],"k":[121]},
+"F11":{"v":["VK_F11"],"k":[122]},
+"F12":{"v":["VK_F12"],"k":[123]},
+"F13":{"v":["VK_F13"],"k":[124]},
+"F14":{"v":["VK_F14"],"k":[125]},
+"F15":{"v":["VK_F15"],"k":[126]},
+"F16":{"v":["VK_F16"],"k":[127]},
+"F17":{"v":["VK_F17"],"k":[128]},
+"F18":{"v":["VK_F18"],"k":[129]},
+"F19":{"v":["VK_F19"],"k":[130]},
+"F20":{"v":["VK_F20"],"k":[131]},
+"ChannelDown":{"v":["APPCOMMAND_MEDIA_CHANNEL_DOWN"],"k":[52]},
+"ChannelUp":{"v":["APPCOMMAND_MEDIA_CHANNEL_UP"],"k":[51]},
+"MediaFastForward":{"v":["APPCOMMAND_MEDIA_FAST_FORWARD"],"k":[49]},
+"MediaPause":{"v":["APPCOMMAND_MEDIA_PAUSE"],"k":[47]},
+"MediaPlay":{"v":["APPCOMMAND_MEDIA_PLAY"],"k":[46]},
+"MediaPlayPause":{"v":["VK_MEDIA_PLAY_PAUSE","APPCOMMAND_MEDIA_PLAY_PAUSE"],"k":[179,14]},
+"MediaRecord":{"v":["APPCOMMAND_MEDIA_RECORD"],"k":[48]},
+"MediaRewind":{"v":["APPCOMMAND_MEDIA_REWIND"],"k":[50]},
+"MediaStop":{"v":["VK_MEDIA_STOP","APPCOMMAND_MEDIA_STOP"],"k":[178,13]},
+"MediaTrackNext":{"v":["VK_MEDIA_NEXT_TRACK","APPCOMMAND_MEDIA_NEXTTRACK"],"k":[176,11]},
+"MediaTrackPrevious":{"v":["VK_MEDIA_PREV_TRACK","APPCOMMAND_MEDIA_PREVIOUSTRACK"],"k":[177,12]},
+"AudioBassDown":{"v":["APPCOMMAND_BASS_DOWN"],"k":[19]},
+"AudioBassBoostToggle":{"v":["APPCOMMAND_BASS_BOOST"],"k":[20]},
+"AudioBassUp":{"v":["APPCOMMAND_BASS_UP"],"k":[21]},
+"AudioTrebleDown":{"v":["APPCOMMAND_TREBLE_DOWN"],"k":[22]},
+"AudioTrebleUp":{"v":["APPCOMMAND_TREBLE_UP"],"k":[23]},
+"AudioVolumeDown":{"v":["VK_VOLUME_DOWN","APPCOMMAND_VOLUME_DOWN"],"k":[174,9]},
+"AudioVolumeMute":{"v":["VK_VOLUME_MUTE","APPCOMMAND_VOLUME_MUTE"],"k":[173,8]},
+"AudioVolumeUp":{"v":["VK_VOLUME_UP","APPCOMMAND_VOLUME_UP"],"k":[175,10]},
+"MicrophoneToggle":{"v":["APPCOMMAND_MIC_ON_OFF_TOGGLE"],"k":[44]},
+"MicrophoneVolumeDown":{"v":["APPCOMMAND_MICROPHONE_VOLUME_DOWN"],"k":[25]},
+"MicrophoneVolumeMute":{"v":["APPCOMMAND_MICROPHONE_VOLUME_MUTE"],"k":[24]},
+"MicrophoneVolumeUp":{"v":["APPCOMMAND_MICROPHONE_VOLUME_UP"],"k":[26]},
+"MediaApps":{"v":["VK_APPS"],"k":[93]},
+"ZoomToggle":{"v":["VK_ZOOM"],"k":[251]},
+"SpeechCorrectionList":{"v":["APPCOMMAND_CORRECTION_LIST"],"k":[45]},
+"SpeechInputToggle":{"v":["APPCOMMAND_DICTATE_OR_COMMAND_CONTROL_TOGGLE"],"k":[43]},
+"LaunchCalculator":{"v":["APPCOMMAND_LAUNCH_APP2"],"k":[18]},
+"LaunchMail":{"v":["VK_LAUNCH_MAIL","APPCOMMAND_LAUNCH_MAIL"],"k":[180,15]},
+"LaunchMediaPlayer":{"v":["VK_LAUNCH_MEDIA_SELECT","APPCOMMAND_LAUNCH_MEDIA_SELECT"],"k":[181,16]},
+"LaunchMyComputer":{"v":["APPCOMMAND_LAUNCH_APP1"],"k":[17]},
+"LaunchApplication1":{"v":["VK_LAUNCH_APP1","APPCOMMAND_LAUNCH_APP1"],"k":[182,17]},
+"LaunchApplication2":{"v":["VK_LAUNCH_APP2","APPCOMMAND_LAUNCH_APP2"],"k":[183,18]},
+"BrowserBack":{"v":["VK_BROWSER_BACK","APPCOMMAND_BROWSER_BACKWARD"],"k":[166,1]},
+"BrowserFavorites":{"v":["VK_BROWSER_FAVORITES","APPCOMMAND_BROWSER_FAVORITES"],"k":[171,6]},
+"BrowserForward":{"v":["VK_BROWSER_FORWARD","APPCOMMAND_BROWSER_FORWARD"],"k":[167,2]},
+"BrowserHome":{"v":["VK_BROWSER_HOME","APPCOMMAND_BROWSER_HOME"],"k":[172,7]},
+"BrowserRefresh":{"v":["VK_BROWSER_REFRESH","APPCOMMAND_BROWSER_REFRESH"],"k":[168,3]},
+"BrowserSearch":{"v":["VK_BROWSER_SEARCH","APPCOMMAND_BROWSER_SEARCH"],"k":[170,5]},
+"BrowserStop":{"v":["VK_BROWSER_STOP","APPCOMMAND_BROWSER_STOP"],"k":[169,4]},
+"Decimal":{"v":["VK_DECIMAL"],"k":[110]},
+"Multiply":{"v":["VK_MULTIPLY"],"k":[106]},
+"Add":{"v":["VK_ADD"],"k":[107]},
+"Divide":{"v":["VK_DIVIDE"],"k":[111]},
+"Subtract":{"v":["VK_SUBTRACT"],"k":[109]},
+"Separator":{"v":["VK_SEPARATOR"],"k":[108]}};
+var g_mpos={x:0,y:0};
+var g_kb_down={};
+var g_kb_changed={};
 function start(){
+  document.documentElement.style.overflow='hidden';
+  document.body.scroll="no";
+  g_keys.mbLeft={k:[257]};
+  g_keys.mbRight={k:[258]};
+  g_keys.mbMiddle={k:[259]};
+  let mid2key=['mbLeft','mbRight','mbMiddle'];
+  for(let k='a'.charCodeAt(0);k<='z'.charCodeAt(0);k++)g_keys[String.fromCharCode(k)]={k:[String.fromCharCode(k).toUpperCase().charCodeAt(0)]};
+  for(let k='A'.charCodeAt(0);k<='Z'.charCodeAt(0);k++)g_keys[String.fromCharCode(k)]={k:[k]};
+  document.addEventListener('keydown',function(event){
+    if(event.key in g_keys){
+      let k=g_keys[event.key];
+      k.down=true;
+      k.changed=true;
+      k.k.map(k=>g_kb_down[k]=1);
+      k.k.map(k=>g_kb_changed[k]=1);
+    }
+  });
+  document.addEventListener('keyup',function(event){
+    if(event.key in g_keys){
+      let k=g_keys[event.key];
+      k.down=false;
+      k.changed=true;
+      k.k.map(k=>g_kb_down[k]=0);
+      k.k.map(k=>g_kb_changed[k]=1);
+    }
+  });
+  document.addEventListener('mousemove',function(e){
+    var x=e.pageX;
+    var y=e.pageY;
+    g_mpos={x,y};
+  });
+  document.addEventListener("mousedown",(e)=>{
+    if(e.button>2)return;
+    let k=g_keys[mid2key[e.button]];
+    k.down=true;
+    k.changed=true;
+    k.k.map(k=>g_kb_down[k]=1);
+    k.k.map(k=>g_kb_changed[k]=1);
+  });
+  document.addEventListener("mouseup",(e)=>{
+    if(e.button>2)return;
+    let k=g_keys[mid2key[e.button]];
+    k.down=false;
+    k.changed=true;
+    k.k.map(k=>g_kb_down[k]=0);
+    k.k.map(k=>g_kb_changed[k]=1);
+  });
   Module.ccall('qap_main','int',["int"],[0]);
 }
 
@@ -396,9 +593,11 @@ function loadShader(gl, type, source) {
   return shader;
 }
 function bindTex(qDev,texture){
-  let gl=qDev.gl;
+  let gl=qDev.gl;let tex=g_textures[texture];
+  gl.uniform1f(qDev.prog.uniformLocations.use_tex,tex?1.0:0.0);
+  if(!tex)return;
   gl.activeTexture(gl.TEXTURE0);
-  gl.bindTexture(gl.TEXTURE_2D,g_textures[texture]);
+  gl.bindTexture(gl.TEXTURE_2D,tex);
 }
 g_textures=[null];
 function makeTexture(qDev,w,h,pix){
@@ -429,17 +628,39 @@ function makeTexture(qDev,w,h,pix){
   g_textures.push(texture);
   return tex_id;
 }
-function loadTexture(gl, url) {
-  const texture = gl.createTexture();
+function loadTexture_v2(url){
+  const img=new Image();
+  img.onload=()=>{
+    let c=document.createElement('canvas');
+    let ctx=c.getContext('2d');
+    c.width=img.width;c.height=img.height;
+    ctx.drawImage(img,0,0);
+    let d=ctx.getImageData(0,0,c.width,c.height).data;
+    let n=c.width*c.height*4;
+    let p=Module._malloc(n);
+    for(let i=0;i<n;i++)HEAPU8[p+i]=d[i];
+    //let ptr=allocate(intArrayFromString(url),'i8',ALLOC_NORMAL);
+    //let ptr=stringToNewUTF8(url);
+    let retval=Module.ccall(
+      'qap_on_load_img','number',
+      ['string','number','number','number'],
+      [url,p,c.width,c.height]
+    );
+    //_free(p);  
+  };
+  img.src=url.split("\\").join("/");
+}
+function loadTexture(gl,url) {
+  const texture=gl.createTexture();
   gl.bindTexture(gl.TEXTURE_2D, texture);
-  const level = 0;
-  const internalFormat = gl.RGBA;
-  const width = 1;
-  const height = 1;
-  const border = 0;
-  const srcFormat = gl.RGBA;
-  const srcType = gl.UNSIGNED_BYTE;
-  const pixel = new Uint8Array([0, 0, 255, 255]); // opaque blue
+  const level=0;
+  const internalFormat=gl.RGBA;
+  const width=1;
+  const height=1;
+  const border=0;
+  const srcFormat=gl.RGBA;
+  const srcType=gl.UNSIGNED_BYTE;
+  const pixel=new Uint8Array([0,0,255,255]);
   gl.texImage2D(
     gl.TEXTURE_2D,
     level,
@@ -451,10 +672,9 @@ function loadTexture(gl, url) {
     srcType,
     pixel
   );
-
-  const image = new Image();
-  image.onload = () => {
-    gl.bindTexture(gl.TEXTURE_2D, texture);
+  const image=new Image();
+  image.onload=()=>{
+    gl.bindTexture(gl.TEXTURE_2D,texture);
     gl.texImage2D(
       gl.TEXTURE_2D,
       level,
@@ -463,12 +683,12 @@ function loadTexture(gl, url) {
       srcType,
       image
     );
-    if(isPowerOf2(image.width) && isPowerOf2(image.height)){
+    if(isPowerOf2(image.width)&&isPowerOf2(image.height)){
       gl.generateMipmap(gl.TEXTURE_2D);
     }else{
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+      gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_S,gl.CLAMP_TO_EDGE);
+      gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_T,gl.CLAMP_TO_EDGE);
+      gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_MIN_FILTER,gl.LINEAR);
     }
   };
   image.src=url;
@@ -486,6 +706,7 @@ var g_start_int=setInterval(()=>{
      ('ccall' in Module)&&
      (typeof wasmExports !== 'undefined')
     ){
+    document.body.style="width:100%; height:100%;";
     clearInterval(g_start_int);start();
   };
 },1000);
