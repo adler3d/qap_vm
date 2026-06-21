469d7bb22cde0ab7037ee5238d8798f6d7f5964a
adler3d <adler3d@gmail.com>
2026-06-21 10:54:57 +0300

    upd

--- trash/test2026/tcalc_binding.cpp
+++ trash/test2026/tcalc_binding.cpp
@@ -4,11 +4,9 @@
 namespace py = pybind11;
 
 double eval_expr_py(const std::string& expr, const py::dict& vars) {
-    // Сбор vars в double[]
     /*
     int n = vars.size();
     std::vector<double> args(n);
-    // ... заполнить args ...
 
     CompiledFunc* func = compile_func(expr.c_str(), "<expr>");
     if (!func) throw std::runtime_error("compile error");
@@ -20,7 +18,6 @@ double eval_expr_py(const std::string& expr, const py::dict& vars) {
 
 PYBIND11_MODULE(tcalc, m) {
     m.doc() = "x86-64 JIT expression evaluator";
-    m.def("eval_expr", eval_expr_py, "Компилирует и вычисляет выражение");
-    // опционально:
+    m.def("eval_expr", eval_expr_py, "compile and eval expr");
     // m.def("plot_formula", ...);
 }
